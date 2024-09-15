9804c537802ec36cf38da3947071139c91cdb96a
yuki-kunn <21e1248@andrew.ac.jp>
2024-09-15 10:55:47 +0900

    cors設定

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\ToDoController;
 use App\Http\Controllers\SujiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/hello', [SujiController::class, 'list']);
 Route::get('/suji', [SujiController::class, 'index']);
 Route::post('/suji', [SujiController::class, 'add']);
 Route::get('/todo', [ToDoController::class, 'index']);
 Route::post('/todo', [ToDoController::class, 'add']);
+Route::get('/users', function () {
+    return User::all();
+});
